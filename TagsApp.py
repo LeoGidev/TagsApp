aa073e8b6b5d43f868822e221224e6dda266434f
LeoGidev <leogimenez.dev@gmail.com>
2024-02-25 21:06:11 -0300

    se colocan librerías

--- TagsApp.py
+++ TagsApp.py
@@ -2,3 +2,10 @@ import pandas as pd
 import openpyxl
 import time
 import cv2
+from tkinter import Tk, Label, Text, Button, filedialog, Frame, ttk, Scale, Canvas
+import os
+from ttkthemes import ThemedTk
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+import imageio
