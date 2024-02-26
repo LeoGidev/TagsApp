d952b2412a1120fcf1925cb9c9f2d5d1cdda8fc9
LeoGidev <leogimenez.dev@gmail.com>
2024-02-25 21:07:29 -0300

    se trabaja en clase principal

--- TagsApp.py
+++ TagsApp.py
@@ -9,3 +9,26 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import imageio
+
+
+
+class calculadoraRED:
+    def __init__(self, root):
+        #configuración de ventana
+        self.root = root
+        self.root.title('Tags App')
+        self.root.geometry("860x600")
+        self.root.configure(bg='#414141')
+        
+        #estilos de los frames
+        style = ttk.Style()        
+        self.root.set_theme('equilux')  
+        style.configure('barratop.TFrame', background='#414141')
+        style.configure('modulo.TFrame', background='#414141')
+        
+        #configuración de la prioridad para achicar columnas o rows en el resize de la ventana
+        self.root.columnconfigure(0, weight=0)
+        self.root.columnconfigure(1, weight=0)
+        self.root.rowconfigure(3, weight=1)
+        #Configuración del icono
+        self.root.iconbitmap(os.path.abspath("icon.ico"))
